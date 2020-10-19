3ba087d2724f6179cb88c31e6c1115a168153214
Mikan0417 <ykwo0210@gmail.com>
2020-10-19 18:14:50 +0000

    [add] ジャンル一覧・追加ページの作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,29 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+       flash[:notice] = "ジャンルを追加しました"
+       redirect_to admin_genres_path
+    else
+      @genres = Genre.all
+      render :index
+    end
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
   end
 end
