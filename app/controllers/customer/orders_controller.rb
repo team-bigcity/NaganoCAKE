7e78616ca2fc50d108f9da786119eeeedf307382
EC2 Default User <s21151103@g.rakuno.ac.jp>
2020-10-19 09:22:31 +0000

    start orders_new

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,14 +1,24 @@
 class Customer::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+     @order = Order.find(params[:id])
   end
 
   def new
+     @order = Order.new
   end
 
   def create
+     @order = Order.new(params[:id])
+    if @order.save
+       redirect_to confirm
+    else
+       @order = Order.new
+       render new
+    end
   end
 
   def confirm
@@ -16,4 +26,11 @@ class Customer::OrdersController < ApplicationController
 
   def complete
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:pstcode,:address,:name,:payment_method,:status)
+  end
+
 end
