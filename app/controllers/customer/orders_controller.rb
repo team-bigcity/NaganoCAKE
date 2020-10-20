dd735d80c7cc4f83549882b5e855bf427ded1b7f
EC2 Default User <s21151103@g.rakuno.ac.jp>
2020-10-20 09:15:09 +0000

    [fix]orders_confirm

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -14,7 +14,7 @@ class Customer::OrdersController < ApplicationController
   def create
      @order = Order.new(params[:id])
     if @order.save
-       redirect_to confirm
+       redirect_to comfirm
     else
        @order = Order.new
        render new
@@ -22,6 +22,14 @@ class Customer::OrdersController < ApplicationController
   end
 
   def confirm
+    #   @products = Product.find_by(params[:id])
+    #   @order = Order.find(params[:id])
+    # if @order.save
+    #   redirect_to complete_path
+    # else
+    #   @order = Order.new
+    #   render new
+    # end
   end
 
   def complete
