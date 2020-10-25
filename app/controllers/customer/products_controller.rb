c4b96853137f580ec52a0ca5ef3c5b0fa56e7195
EC2 Default User <ec2-user@ip-172-31-3-30.ec2.internal>
2020-10-25 02:31:31 +0000

    update--top以外ha

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,6 +1,7 @@
 class Customer::ProductsController < ApplicationController
   def top
     @rank_products = OrderProduct.find(OrderProduct.group(:product_id).order('count(amount) desc').limit(4).pluck(:id))
+    # @all_ranks = Product.find(OrderProduct.group(:product_id).order('count(product_id) desc').limit(4).pluck(:product_id))
     @genres = Genre.where(is_active: true)
     @products = Product.all.page(params[:page]).per(4)
     # @product = Product.find(params[:id])
